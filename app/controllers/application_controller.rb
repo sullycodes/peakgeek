fefd5bb7429985fbf95cee08a6f7eacb350782b5
sullycodes <dansullivan19@gmail.com>
2019-08-21 13:17:39 -0400

    app controller, models, views

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < Sinatra::Base
+
+    configure do 
+        set :views, 'app/views'
+        set :public_folder, 'public'
+        enable :sessions
+        set :session_secret, SESSION_SECRET
+    end
+
+    get '/' do
+        erb :'sessions/login'
+    end
+
+end
